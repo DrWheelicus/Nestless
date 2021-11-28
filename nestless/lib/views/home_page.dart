1398f6d03ac574538571b198faddf37cd3308c2b
Hayden MacIntyre <42521312+DrWheelicus@users.noreply.github.com>
2021-11-27 19:42:32 -0500

    Created post-login template

---
+++ nestless/lib/views/home_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:nestless/services/authentication.dart';
+
+class HomePage extends StatefulWidget {
+  final BaseAuth auth;
+  final VoidCallback onSignedOut;
+  final String userId;
+
+  const HomePage(
+      {Key? key,
+      required this.auth,
+      required this.onSignedOut,
+      required this.userId})
+      : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('Home'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
